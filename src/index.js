84467c27d902de2549cc81ba184308a9fbd33fee
Aditya Aggarwal <adi.adag@gmail.com>
2018-03-13 22:44:04 +0530

    added button and play pause functions

--- src/index.js
+++ src/index.js
@@ -1,19 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { ButtonToolbar, MenuItem, DropdownButton } from 'react-bootstrap';
 
-
-// Class Buttons extends React.Component
-// {
-// 	render()
-// 	{
-// 		return(
-// 				<div>
-// 					<button onClick=this.seed>Seed</button>
-// 				</div>
-// 			);
-// 	}
-// }
+class Buttons extends React.Component
+{
+	render()
+	{
+		return(
+				<div className="center">
+					<ButtonToolbar>
+					<button className="btn btn-default" onClick={this.props.playButton}>
+						Play
+					</button>
+					<button className="btn btn-default" onClick={this.props.pauseButton}>
+					  Pause
+					</button>
+					<button className="btn btn-default" onClick={this.props.clear}>
+					  Clear
+					</button>
+					<button className="btn btn-default" onClick={this.props.slow}>
+					  Slow
+					</button>
+					<button className="btn btn-default" onClick={this.props.fast}>
+					  Fast
+					</button>
+					<button className="btn btn-default" onClick={this.props.seed}>
+					  Seed
+					</button>
+					<DropdownButton
+						title="Grid Size"
+						id="size-menu"
+						onSelect={this.handleSelect}
+					>
+						<MenuItem eventKey="1">20x10</MenuItem>
+						<MenuItem eventKey="2">50x30</MenuItem>
+						<MenuItem eventKey="3">70x50</MenuItem>
+					</DropdownButton>
+				</ButtonToolbar>
+				</div>
+			);
+	}
+}
 
 class Box extends React.Component
 {
@@ -38,7 +66,7 @@ class Grid extends React.Component
 	render()
 	{
 
-		const width = (this.props.cols * 16)+1;		
+		const width = (this.props.cols * 14);		
 		let rowsArr=[];
 
 		let boxClass="";
@@ -111,15 +139,68 @@ class Main extends React.Component
 
 		this.setState({
 			gridFull: gridCopy
-		})
+		});
 	}
+
+	playButton = () =>
+	{
+		clearInterval(this.intervalId);
+		this.intervalId=setInterval(this.play,this.speed);
+	}
+
+	slow = () =>
+	{
+		this.speed=1000;
+		this.playButton();
+	}
+
+	pauseButton = () =>
+	{
+		clearInterval(this.intervalId);
+	}
+
+	play = () =>
+	{
+		let g=this.state.gridFull;
+		let g2=arrayClone(this.state.gridFull);
+
+		for (let i = 0; i < this.rows; i++) 
+		{
+		  for (let j = 0; j < this.cols; j++) 
+		  {
+		    let count = 0;
+		    if (i > 0) if (g[i - 1][j]) count++;
+		    if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
+		    if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
+		    if (j < this.cols - 1) if (g[i][j + 1]) count++;
+		    if (j > 0) if (g[i][j - 1]) count++;
+		    if (i < this.rows - 1) if (g[i + 1][j]) count++;
+		    if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
+		    if (i < this.rows - 1 && this.cols - 1) if (g[i + 1][j + 1]) count++;
+		    if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
+		    if (!g[i][j] && count === 3) g2[i][j] = true;
+		  }
+		}
+		this.setState({
+		  gridFull: g2,
+		  generation: this.state.generation + 1
+		});
+
+	}
+
+	componentDidMount()
+	{
+		this.seed();
+	}
+
 
 	render()
 	{
 		return(
 				<div>
 					<h1>The Game of Life</h1>
-					<Grid gridFull={this.state.gridFull} rows={this.rows} cols={this.cols} selectBox={this.selectBox}/>
+					<Buttons playButton={this.playButton} pauseButton={this.pauseButton} slow={this.slow} fast={this.fast} clear={this.clear} seed={this.seed} gridSize={this.gridSize} />
+					<Grid gridFull={this.state.gridFull} rows={this.rows} cols={this.cols} selectBox={this.selectBox}/> 
 					<h2>Generations: {this.state.generation}</h2>	
 				</div>
 			);
